870554514d42b1a423317073d7c9ee3493692523
zink_urq00 <iamklarxz00@gmail.com>
2024-12-30 16:53:55 +0700

    Initial commit

---
+++ OpenTKRendering/Game.cs
@@ -0,0 +1,24 @@
+﻿using OpenTK.Graphics.OpenGL;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+
+namespace OpenTKRendering
+{
+    public class Game : GameWindow
+    {
+        public Game() : base(GameWindowSettings.Default, NativeWindowSettings.Default)
+        {
+        }
+
+        protected override void OnLoad()
+        {
+            GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs args)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            SwapBuffers();
+        }
+    }
+}
---
+++ OpenTKRendering/Program.cs
@@ -0,0 +1,23 @@
+﻿using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTKRendering;
+
+namespace OpenTKBasics
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (Game game = new Game())
+            {
+                game.Run();
+            }
+
+        }
+
+        
+
+    }
+}
